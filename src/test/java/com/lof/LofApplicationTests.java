d8c26622d5af283376daed4011194b010ef677ec
devhoya97 <diajbh123@gmail.com>
2025-02-04 13:32:09 +0900

    chore: 프로젝트 환경 셋업

---
+++ src/test/java/com/lof/LofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
