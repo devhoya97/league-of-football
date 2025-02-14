0f5911ac30a302e63a27157a9e7157595c990dbc
devhoya97 <diajbh123@gmail.com>
2025-02-14 16:42:01 +0900

    test: MemberController 테스트코드에서 예외상황 테스트 변경

--- src/main/java/com/lof/member/controller/SignUpRequest.java
+++ src/main/java/com/lof/member/controller/SignUpRequest.java
@@ -1,13 +1,14 @@
 package com.lof.member.controller;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequest(
-        @NotBlank(message = "회원 아이디를 입력해주세요.")
+        @NotNull(message = "회원 아이디를 입력해주세요.")
         @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,20}$", message = "아이디는 5~20자의 영문과 숫자로 구성되어야 하며, 숫자로만 이루어질 수 없습니다.")
         String loginId,
-        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @NotNull(message = "비밀번호를 입력해주세요.")
         @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,20}$", message = "비밀번호는 8~20자이며, 대문자, 소문자, 숫자, 특수문자를 최소 하나씩 포함해야 합니다.")
         String password
 ) {
--- src/test/java/com/lof/member/controller/MemberControllerTest.java
+++ src/test/java/com/lof/member/controller/MemberControllerTest.java
@@ -1,38 +1,88 @@
 package com.lof.member.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.lof.global.exception.ErrorCode;
 import com.lof.member.fixture.MemberFixture;
-import com.lof.member.repository.MemberRepository;
+import com.lof.member.service.MemberService;
 
-// TODO: WebMvcTest에서는 HTTP 요청을 request DTO로 변환하는 과정을 테스트하고
-//  여기서는 그 뒷단을 테스트하게 되면 RestAssured를 활용한 통합테스트가 필요없는게 아닐까?
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
-    @Autowired
-    private MemberController memberController;
+    // 애플리케이션컨텍스트 재사용을 위해 @MockitoBean이 아닌 @TestConfiguration 사용
+    // https://tech.kakaopay.com/post/mock-test-code/
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public MemberService memberService() {
+            return mock(MemberService.class);
+        }
+    }
 
     @Autowired
-    private MemberRepository memberRepository;
+    private MockMvc mockMvc;
 
     @Test
-    @DisplayName("회원 가입을 위한 검증에 통과하면, 회원가입에 성공한다.")
-    void signUp() {
+    @DisplayName("명세에 맞게 회원 가입 API를 호출하면, 201 응답코드를 반환한다.")
+    void signUp() throws Exception {
         // given
-        SignUpRequest signUpRequest = MemberFixture.createSignUpRequest(MemberFixture.VALID_LOGIN_ID, MemberFixture.VALID_PASSWORD);
-        int initialSize = memberRepository.findAll().size();
+        String signUpRequest = MemberFixture.createSignUpRequestString(MemberFixture.VALID_LOGIN_ID, MemberFixture.VALID_PASSWORD);
 
-        // when
-        memberController.signUp(signUpRequest);
+        // when & then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signUpRequest))
+                .andExpect(status().isCreated());
+    }
 
-        // then
-        assertThat(memberRepository.findAll().size()).isEqualTo(initialSize + 1);
+    @ParameterizedTest
+    @DisplayName("회원 아이디 규칙에 벗어나면 예외가 발생한다.")
+    @ValueSource(strings = {"aaaa", "12345", "a123456789a1234567890"})
+    void signUpFailByLoginId(String loginId) throws Exception {
+        // given
+        String signUpRequest = MemberFixture.createSignUpRequestString(loginId, MemberFixture.VALID_PASSWORD);
+
+        // when & then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signUpRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("code", is(ErrorCode.INVALID_USER_INPUT.toString())))
+                .andExpect(jsonPath("message", is("아이디는 5~20자의 영문과 숫자로 구성되어야 하며, 숫자로만 이루어질 수 없습니다.")));
+    }
+
+    @ParameterizedTest
+    @DisplayName("회원 비밀번호 규칙에 벗어나면 예외가 발생한다.")
+    @ValueSource(strings = {"12345678", "a1234567", "Aa123456", "Aa1234!", "A123456789a123456789!"})
+    void signUpFailByPassword(String password) throws Exception {
+        // given
+        String signUpRequest = MemberFixture.createSignUpRequestString(MemberFixture.VALID_LOGIN_ID, password);
+
+        // when & then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signUpRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("code", is(ErrorCode.INVALID_USER_INPUT.toString())))
+                .andExpect(jsonPath("message", is("비밀번호는 8~20자이며, 대문자, 소문자, 숫자, 특수문자를 최소 하나씩 포함해야 합니다.")));
     }
 }
